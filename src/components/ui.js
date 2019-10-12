0e746f417e010427ef417af3c3115e2882b504ad
Tom Greuter <tomgreuter@xs4all.nl>
2019-10-12 22:26:16 +0200

    Another window object fix

--- src/components/ui.js
+++ src/components/ui.js
@@ -40,7 +40,7 @@ export const GlobalStyles = () => {
   // window.location.reload();
 
   const styles =
-    window &&
+    typeof window !== "undefined" &&
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
       ? cssDark
